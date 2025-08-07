907db18008d5785415332b533d7a0863d576a8bc
Port9211 <129165189+Port9211@users.noreply.github.com>
2025-08-07 17:55:44 +0530

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,16 +1,20 @@
 module "eks" {
-  source = "terraform-aws-modules/eks/aws"
-   cluster_name  = "my-eks-cluster"
-   cluster_version = "1.27"
-  subnets = module.vpc.private_subnets
-  vpc_id = module.vpc.vpc_id
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.4" # Use latest compatible version (you can adjust)
 
-  node_groups = {
-    default  = {
-      desired_capacity = 2
-      max_capacity     = 3
-      min_capacity     = 1
-      instance_type   = ["t3.medium"]
+  cluster_name    = "my-eks-cluster"
+  cluster_version = "1.27"
+
+  vpc_id  = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size = 2
+      max_size     = 3
+      min_size     = 1
+
+      instance_types = ["t3.medium"]
     }
   }
 }
